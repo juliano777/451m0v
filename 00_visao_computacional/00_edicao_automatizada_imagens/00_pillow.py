bff312eb9672a5735e0bf234ff752d8fcfafd8b6
Juliano Atanazio <juliano777@gmail.com>
2023-11-22 17:12:25 -0300

    Nova organização

--- 00_visao_computacional/00_edicao_automatizada_imagens/00_pillow.py
+++ 00_visao_computacional/00_edicao_automatizada_imagens/00_pillow.py
@@ -1,10 +1,7 @@
 from PIL.Image import open as PIL_Image_open
-
+from PIL.Image import Transpose as PIL_Image_Transpose
 # Cria um objeto de imagem
 img = PIL_Image_open('img/image.jpg')
-
-# Exibe a imagem
-img.show()
 
 # Informações da imagem
 formato = img.format
@@ -22,8 +19,30 @@ Largura = {largura}
 Altura = {altura}
 '''
 
-img
-
-
 # Exibir informações
 print(img_info)
+
+# Exibe a imagem
+# img.show()
+
+# Redimensionar a imagem em 50%
+novo_tamanho = (
+    round(largura / 10),
+    round(altura / 10)
+    )
+
+img2 = img.resize(size=novo_tamanho)
+
+crop_data = (0, 0, 10, 200)
+img.crop()
+
+img2.save('/tmp/teste.jpg')
+#img.show()
+
+img.rotate(15)
+
+img.transpose(PIL_Image_Transpose.ROTATE_180)
+
+
+img.thumbnail((50,50))
+img
