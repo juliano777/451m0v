bbb64f7b0836f6ea60c92b7096dc928e91b0d786
Juliano Atanazio <juliano777@gmail.com>
2023-11-22 20:14:34 -0300

    phono2

--- 00_visao_computacional/00_edicao_automatizada_imagens/00_pillow.py
+++ 00_visao_computacional/00_edicao_automatizada_imagens/00_pillow.py
@@ -1,7 +1,9 @@
 from PIL.Image import open as PIL_Image_open
 from PIL.Image import Transpose as PIL_Image_Transpose
+from PIL.ImageOps import invert as PIL_ImageOps_invert
+
 # Cria um objeto de imagem
-img = PIL_Image_open('img/image.jpg')
+img = PIL_Image_open('img/phono.jpg')
 
 # Informações da imagem
 formato = img.format
@@ -25,24 +27,43 @@ print(img_info)
 # Exibe a imagem
 # img.show()
 
-# Redimensionar a imagem em 50%
+# Tupla com novos valores reduzindo em 50%
 novo_tamanho = (
-    round(largura / 10),
-    round(altura / 10)
+    round(largura / 2),
+    round(altura / 2)
     )
 
+# Cria a nova imagem com um novo tamanho
 img2 = img.resize(size=novo_tamanho)
 
-crop_data = (0, 0, 10, 200)
-img.crop()
+# Tupla com os valores para crop
+crop_data = (
+             5,  # Posição inicial X
+             55,  # Posição inicial Y
+             264,  # Quantos pixels X para manter
+             345  # Quantos pixels Y para manter
+             )
 
-img2.save('/tmp/teste.jpg')
-#img.show()
+# Fazer o corte da imagem com o método crop e os dados da tupla
+img2 = img2.crop(crop_data)
 
-img.rotate(15)
+# Converter para grayscale
+img2 = img2.convert('L')
 
-img.transpose(PIL_Image_Transpose.ROTATE_180)
+# Inverter as cores da imagem
+img2 = PIL_ImageOps_invert(img2)
 
+# Fazer a transposição rotacionando 180°
+img2 = img2.transpose(PIL_Image_Transpose.ROTATE_180)
 
-img.thumbnail((50,50))
-img
+# Rotacionar 15°
+img2 = img2.rotate(15)
+
+# Salvar a imagem
+img2.save('img/phono2.jpg')
+
+# Redimensionar mantendo a proporção
+# img2 = img.thumbnail((50,50))
+
+# Exibir a imagem final
+img2.show()
